2b3d9fa1bf4f16b400b1edac6131aa29103460e7
Z <abdrashovv@gmail.com>
2023-11-08 12:45:31 +0600

    Upgrading to 10 from 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,30 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
-        $this->app->bind('path.public', function() {
-            return base_path().'/public_html';
-        });
+        //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        //
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,13 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-   return $request->user();
+    return $request->user();
 });
 
 Route::post('login', [LoginController::class, 'login']);
@@ -27,7 +27,7 @@ Route::get('post', [PostController::class, 'index']);
 Route::get('post/{id}/show', [PostController::class, 'show']);
 
 Route::group(['middleware' => 'jwt.verify'], function(){
-	Route::post('post/store', [PostController::class, 'store']);
-	Route::put('post/{id}/update', [PostController::class, 'update']);
-	Route::delete('post/{id}/destroy', [PostController::class, 'destroy']);
+    Route::post('post/store', [PostController::class, 'store']);
+    Route::put('post/{id}/update', [PostController::class, 'update']);
+    Route::delete('post/{id}/destroy', [PostController::class, 'destroy']);
 });
